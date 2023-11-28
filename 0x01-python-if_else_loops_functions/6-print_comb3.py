1f170b9bbffe29da0ff64746466319025d71021e
DANIEL AMOAKO KODUA <officialjwise20@gmail.com>
2023-11-28 05:49:19 -0800

    Print comb3 file

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-"""Print all possible different combinations of two digits in ascending order.
-    The two digits must be different - 01 and 10 are considered identical."""
-
-for first_digit in range(10):
-    for second_digit in range(first_digit + 1, 10):
-        separator = ", "
-
-        if first_digit < 8 or (first_digit == 8 and second_digit < 9):
-            print(f"{first_digit}{second_digit}", end=separator)
-        else:
-            print(f"{first_digit}{second_digit}")
+for i in range(0, 8):
+    for j in range(a + 1, 10):
+        print("{:d}{:d}".format(i, j), end=', ')
+print("{:d}{:d}".format(i + 1, j))
