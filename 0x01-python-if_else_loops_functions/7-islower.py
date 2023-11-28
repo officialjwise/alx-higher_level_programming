b05bda4b3308eafd9b97925c0270be1e7d7b3077
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-11-28 13:53:15 +0000

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    daniel = ord(daniel)
-    if daniel >= 97 and daniel <= 122:
+    """Checks for lowercase charactors."""
+    if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
         return False
