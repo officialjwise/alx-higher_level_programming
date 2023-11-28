116c52fb91952f7bf2a4c650a3ec4b96d7bdc834
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-11-28 13:33:23 +0000

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-
 for number in range(100):
-    print(f"{number:02d}", end=", " if number < 99 else "\n")
+    separator = ", " if number < 99 else "\n"
+    print("{:02d}".format(number), end=separator)
