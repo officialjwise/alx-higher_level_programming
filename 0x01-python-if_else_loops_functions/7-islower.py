17d97268024dd062b9b67400fb6795c5bc817d3e
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-11-28 13:52:14 +0000

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-
 def islower(c):
-    """Check if character c is lowercase."""
-    # Let's check if the ASCII value of c is in the range of lowercase letters
-    return ord('a') <= ord(c) <= ord('z')
-
-
-# Testing the function
-print(islower('a'))  # Returns True
-print(islower('A'))  # Returns False
-print(islower('1'))  # Returns False
+    daniel = ord(daniel)
+    if daniel >= 97 and daniel <= 122:
+        return True
+    else:
+        return False
