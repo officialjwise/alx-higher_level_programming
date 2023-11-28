f80a775f5bc958bfe8c14087d9e4f0cd86d06e20
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-11-28 13:25:10 +0000

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    print("{}".format(chr(i)), end="")
+for chars in range(97, 123):
+    if chars != 113 and chars != 101:
+        print("{:c}".format(chars), end="")
