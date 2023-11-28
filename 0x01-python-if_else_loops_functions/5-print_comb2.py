3ffbd6ed4db71e1ca68c068c4e17a86ed67f3baf
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-11-28 14:36:10 +0000

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-
-for number in range(100):
-    print(f"{number:02d}", end=", " if number < 99 else "\n")
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
