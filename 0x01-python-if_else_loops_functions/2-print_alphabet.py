9087fff2e700ebd4fc455de816ffd8e3353867e4
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-11-28 13:03:48 +0000

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
-output_str = ""
-for char_code in range(ord('a'), ord('z') + 1):
-    output_str += chr(char_code)
-print("{}".format(output_str), end="")
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="")
