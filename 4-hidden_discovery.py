8a2b7d5af369976fe8a4d5c90a54257d0c2c6a86
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-12-15 09:36:02 +0000

    Delete 4-hidden_discovery.py

--- 4-hidden_discovery.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from hidden_4 import *
-    names = dir()
-    for i in range(0, len(names)):
-        if names[i][0:2] != "__":
-            print("{}".format(names[i]))
