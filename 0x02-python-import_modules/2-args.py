6a06591dd96e08bd2106f2c17cb9dd7626a3ebbf
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-12-15 09:43:34 +0000

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,28 +1,14 @@
 #!/usr/bin/python3
-
-from sys import argv
-
-
-def print_args() -> None:
-    """
-    Prints the number of command line arguments with
-    each argument on a new line
-    """
-    # get the command line arguments without the filename
-    size = len(argv[1:])
-
-    print(f"{size} argument{'s' if size == 0 or size > 1 else ''}", end="")
-
-    # check for when there is no arguments passed
-    if not argv[1:]:
-        print(".")
-        return
-
-    # we had arguments passed on the command line, let's print them
-    print(":")
-    for position, arg in enumerate(argv[1:], start=1):
-        print(f"{position}: {arg}")
-
-
 if __name__ == "__main__":
-    print_args()
+    """Print the number of and list of arguments"""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
