8d31a81ebf1a841746dcccdd5f17d4de04891a6f
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-12-15 09:44:37 +0000

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,26 +1,9 @@
 #!/usr/bin/python3
-
-from sys import argv
-
-
-def infinite_add() -> None:
-    """
-    Prints the result of the addition of all arguments
-    """
-    # handle when no numbers are given
-    if not argv[1:]:
-        print("0")
-        return
-
-    sum = 0
-    for operand in argv[1:]:
-        try:
-            sum += int(operand)
-        except ValueError:
-            print("Expected an integer!")
-            return
-    print(sum)
-
-
 if __name__ == "__main__":
-    infinite_add()
+    """Print the addition of all arguments"""
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
